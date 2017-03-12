866e2d4aab519ccd9e83bb152ec98bc84d828c7d
kicumkicum <kicumkicum@gmail.com>
2017-03-13 01:03:50 +0700

    Update structure of database

--- scripts/init-database.js
+++ scripts/init-database.js
@@ -10,19 +10,20 @@ rl.question('Tables will be dropped. You are sure? (y/N): ', function(ask) {
 		Promise.resolve()
 			.then(() => database.query('DROP TABLE books'))
 			.then(() => database.query('DROP TABLE authors'))
-			.then(() => database.query('DROP TABLE library'))
+			.catch((err) => console.warn(String(err)))
 
-			.then(() => database.query('CREATE TABLE books (id INTEGER, title TEXT);'))
-			.then(() => database.query('INSERT INTO books (id, title) VALUES (1, \'Tom Sawyer\');'))
-			.then(() => database.query('INSERT INTO books (id, title) VALUES (2, \'Geklberi Finn\');'))
+			.then(() => database.query('CREATE TABLE authors (id SERIAL, name TEXT);'))
+			.then(() => database.query('INSERT INTO authors (name) VALUES ("Mark Twain");'))
 
-			.then(() => database.query('CREATE TABLE authors (id INTEGER, name TEXT);'))
-			.then(() => database.query('INSERT INTO authors (id, name) VALUES (1, \'Mark Twain\');'))
+			.then(() => database.query('CREATE TABLE books (id SERIAL, title TEXT, authors TEXT[]);'))
+			.then(() => database.query('INSERT INTO books (title, authors) VALUES ("Tom Sawyer", ARRAY["Mark Twain"]);'))
+			.then(() => database.query('INSERT INTO books (title, authors) VALUES ("Geklberi Finn", ARRAY["Mark Twain"]);'))
 
-			.then(() => database.query('CREATE TABLE library (id INTEGER, author_id INTEGER, book_id INTEGER);'))
-			.then(() => database.query('INSERT INTO library (id, author_id, book_id) VALUES (1, 1, 1);'))
-			.then(() => database.query('INSERT INTO library (id, author_id, book_id) VALUES (2, 1, 2);'))
-			.then(() => process.exit(0));
+			.then(() => process.exit(0))
+			.catch((err) => {
+				console.error(err);
+				process.exit(1);
+			});
 	} else {
 		process.exit(0);
 	}
