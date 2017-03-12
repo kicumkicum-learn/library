a8753d1c7e12b3a3e2727c355673ffc4189b0d03
kicumkicum <kicumkicum@gmail.com>
2017-03-12 22:59:57 +0700

    Drop tables with run init database

--- scripts/init-database.js
+++ scripts/init-database.js
@@ -1,15 +1,29 @@
 const DataBase = require('../lib/data-base');
 const databaseConfig = require('../config').database;
+const readLine = require('readline');
 
 const database = new DataBase(databaseConfig);
+const rl = readLine.createInterface(process.stdin, process.stdout);
 
-Promise.resolve()
-	.then(() => database.query('CREATE TABLE books (id INTEGER, title TEXT);'))
-	.then(() => database.query('INSERT INTO books (id, title) VALUES (1, \'Tom Sawyer\');'))
-	.then(() => database.query('INSERT INTO books (id, title) VALUES (2, \'Geklberi Finn\');'))
+rl.question('Tables will be dropped. You are sure? (y/N): ', function(ask) {
+	if (ask === 'y') {
+		Promise.resolve()
+			.then(() => database.query('DROP TABLE books'))
+			.then(() => database.query('DROP TABLE authors'))
+			.then(() => database.query('DROP TABLE library'))
 
-	.then(() => database.query('CREATE TABLE authors (id INTEGER, name TEXT);'))
-	.then(() => database.query('INSERT INTO authors (id, name) VALUES (1, \'Mark Twain\');'))
+			.then(() => database.query('CREATE TABLE books (id INTEGER, title TEXT);'))
+			.then(() => database.query('INSERT INTO books (id, title) VALUES (1, \'Tom Sawyer\');'))
+			.then(() => database.query('INSERT INTO books (id, title) VALUES (2, \'Geklberi Finn\');'))
 
-	.then(() => database.query('CREATE TABLE library (id INTEGER, authorId INTEGER, bookId INTEGER);'))
-	.then(() => database.query('INSERT INTO library (id, authorId, bookId) VALUES (1, 1, 1);'));
+			.then(() => database.query('CREATE TABLE authors (id INTEGER, name TEXT);'))
+			.then(() => database.query('INSERT INTO authors (id, name) VALUES (1, \'Mark Twain\');'))
+
+			.then(() => database.query('CREATE TABLE library (id INTEGER, author_id INTEGER, book_id INTEGER);'))
+			.then(() => database.query('INSERT INTO library (id, author_id, book_id) VALUES (1, 1, 1);'))
+			.then(() => database.query('INSERT INTO library (id, author_id, book_id) VALUES (2, 1, 2);'))
+			.then(() => process.exit(0));
+	} else {
+		process.exit(0);
+	}
+});
