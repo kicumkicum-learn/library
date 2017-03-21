f958a29671fbaafd30ed6f2f3476d53c24c3f6b5
kicumkicum <kicumkicum@gmail.com>
2017-03-21 10:54:22 +0700

    Add config sample

---
+++ config.sample.js
@@ -0,0 +1,17 @@
+const config = {
+	ui: {
+		port: 1337
+	},
+	server: {
+		port: 1338
+	},
+	database: {
+		user: 'user',
+		password: 'password',
+		database: 'database',
+		host: 'localhost',
+		port: 5432
+	}
+};
+
+export default config;
